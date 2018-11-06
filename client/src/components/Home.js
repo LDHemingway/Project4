bb637cb978907420d88408a61bfe1effc4d3bce1
Lauren Hemingway <lauren_hemingway12@hotmail.com>
2018-11-06 18:52:22 -0500

    trying to get pets to display in home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,64 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import axios from 'axios'
 
 export default class Home extends Component {
+  state = {
+    pets: [
+      {
+        status: "",
+        age: "",
+        size: "",
+        image_url: "",
+        name: "",
+        sex: "",
+        id: "",
+        description: "",
+        animal: "",
+        address: "",
+        city: "",
+        state: "",
+        zip: "",
+        phone: ""
+      }
+    ]
+  };
+
+  getPets = async () => {
+    const response = await axios.get('http://api.petfinder.com/pet.find?format=json&key=975abed7430683f27f2e11f386b41692&location={30308}&animal={dog}')
+    return response.data
+  }
+
+
+
+  componentDidMount = async () => {
+    const response = await this.getPets()
+    //axios
+    //set response
+
+    const allPetsResponse = response.data.petfinder.pets
+    const mappedPets = allPetsResponse.map(pet => {
+      return {
+        status: pet.status['$t'],
+        age: pet.age['$t'],
+        size: pet.size['$t'],
+        image_url: pet.media.photos.photo['$t'],
+        id: pet.id['$t'],
+        name: pet.name['$t'],
+        sex: pet.sex['$t'],
+        description: pet.description['$t'],
+        animal: pet.animal['$t'],
+        address: pet.contact.address1['$t'],
+        city: pet.contact.city['$t'],
+        zip: pet.contact.zip['$t'],
+        phone: pet.contact.phone
+
+      }
+    })
+
+    this.setState({ pets: mappedPets })
+  }
+
   render() {
-    return (
-      <div>
-        
-      </div>
-    )
+    return <div />;
   }
 }
