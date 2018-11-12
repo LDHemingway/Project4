357a2607e371e9142009ee6279f0ee087d6b7798
Lauren Hemingway <lauren_hemingway12@hotmail.com>
2018-11-12 16:17:11 -0500

    added overlays on hover for user images

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link, Redirect } from 'react-router-dom'
 import axios from 'axios'
 import styled from 'styled-components'
+import NavBar from "../sharedcomponents.js/NavBar";
 
 const StyledForm = styled.form`
   margin: 0 auto;
@@ -13,11 +14,68 @@ const StyledForm = styled.form`
   justify-content: space-between;
 `
 
+const StyledFormContainer = styled.div`
+  background: lightgray;
+  width: 40vw;
+  margin: 0 auto;
+  border-radius: 15px;
+
+  h4 {
+    text-align: center;
+    padding-top: 40px;
+    padding-bottom: -10px;
+    font-size: 20px;
+    color: #7e7e7f;
+    text-transform: lowercase;
+    letter-spacing: 0.2em;
+    font-weight: 200;
+  }
+`
+const StyledDivContainer = styled.div`
+  position: relative;
+  width: 100%;
+
+  .image {
+    display: block;
+    width: 100%;
+    height: auto;
+  }
+
+  .overlay {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 100%;
+    width: 100%;
+    opacity: 0;
+    transition: .3s ease;
+    background-color: darkgray;
+    border-radius: 15px;
+  }
+
+  :hover .overlay{
+    opacity: 0.7;
+  }
+
+  .text {
+    color: white;
+    font-size: 4em;
+    font-weight: 600;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    -webkit-transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+    text-align: center;
+  }
+`
+
 const StyledUser = styled.span`
   width: 10vw;
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
+  flex: 33.33%;
 `
 
 const StyledInput = styled.input`
@@ -27,8 +85,19 @@ const StyledInput = styled.input`
 `
 const StyledImage = styled.img`
   width: 30vw;
-  border-radius: 15%;
   max-width: 275px;
+  border-radius: 15px;
+
+  :hover {
+
+  }
+`
+
+const StyledUserList = styled.div`
+  background: white;
+  margin: 0 auto;
+  display: flex;
+  width: 50vw;
 `
 
 
@@ -80,25 +149,35 @@ export default class Home extends Component {
     }
     const userslist = this.state.users.map((user, i) => {
       return (
+        <StyledUserList>
         <span key={i}>
         <StyledUser>
-        <h4>{user.name}</h4>
-        <Link to={`/users/${user.id}`}><StyledImage src={user.image_url} alt={user.name} /></Link>
+        <StyledDivContainer>
+        <Link to={`/users/${user.id}`}><StyledImage src={user.image_url} alt={user.name} class="image"/>
+        <div class="overlay">
+          <div class="text">{user.name}</div>
+        </div>
+        </Link>
+        </StyledDivContainer>
         </StyledUser>
         </span>
+        </StyledUserList>
       )
     })
     return (
     <div>
+      <NavBar />
       Select From List of Users
       {userslist}
-      OR Create New User
+      <StyledFormContainer>
+        <h4>Create New User</h4>
       <StyledForm onSubmit={this.onSubmit}>
         <StyledInput type='text' name="name" placeholder='First Name' value={this.state.newUser.name} onChange={this.handleChange}/>
         <StyledInput type="text" name="location" placeholder="City, State" value={this.state.newUser.location} onChange={this.handleChange}/>
         <StyledInput type="text" name="image_url" placeholder="Image URL" value={this.state.newUser.image_url} onChange={this.handleChange}/>
         <StyledInput type="submit" value="Create New User" />
       </StyledForm>
+      </StyledFormContainer>
     </div>
     )
   }
--- client/src/sharedcomponents.js/navbar.js
+++ client/src/sharedcomponents.js/navbar.js
@@ -3,13 +3,27 @@ import styled from 'styled-components'
 
 const StyledNav = styled.div`
     width: 100%;
+    height: 50px;
+    background: lightgray;
+    display: flex;
+    justify-content: space-between;
+
+    h4 {
+      color: white;
+      font-size: 25px;
+      padding-bottom: 10px;
+    }
+    img {
+      
+    }
 `
 
 export default class navbar extends Component {
   render() {
     return (
       <StyledNav>
-        <h4>Project Name</h4>
+        <h4>Fur Babies</h4>
+        <img src='http://www.acelerastartups.com/br/wp-content/themes/yeti-bootstrap/templates-land-pages/assets/plugins/ionicons/src/ios-paw.svg' alt='paw' />
       </StyledNav>
     )
   }
