d2476362c315e0d747b7b0760d476f143f5f8c1c
Lauren Hemingway <lauren_hemingway12@hotmail.com>
2018-11-13 11:24:16 -0500

    photos are showing

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -29,20 +29,20 @@ export default class SearchBar extends Component {
     searchresults: [],
     pets: [
       {
-          status: "",
-          age: "",
-          size: "",
-          image_url: "",
-          name: "",
-          sex: "",
-          id: "",
-          description: "",
-          animal: "",
-          address: "",
-          city: "",
-          state: "",
-          zip: "",
-          phone: ""
+        status: "",
+        age: "",
+        size: "",
+        image_url: "",
+        name: "",
+        sex: "",
+        id: "",
+        description: "",
+        animal: "",
+        address: "",
+        city: "",
+        state: "",
+        zip: "",
+        phone: ""
       }
     ],
     savedPet: {
@@ -62,11 +62,11 @@ export default class SearchBar extends Component {
       phone: ''
     },
     mappedPets: []
-  
+
   }
 
-  handleChange =(event) => {
-    this.setState({ [event.target.name]: event.target.value})
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
   }
 
   onSubmit = async (event) => {
@@ -74,13 +74,13 @@ export default class SearchBar extends Component {
     let location = this.state.location
     let animal = this.state.animal
     let results = await axios.get(`/api/pets?location=${location}&animal=${animal}`)
-    this.setState({searchresults: results.data.petfinder.pets.pet })
-    console.log(this.state.searchresults)
+    this.setState({ searchresults: results.data.petfinder.pets.pet })
+    console.log('search results', this.state.searchresults[0].media.photos.photo[2].$t)
   }
 
   savePet = async (event) => {
     event.preventDefault()
-    const savedPet = {...this.state.savedPet}
+    const savedPet = { ...this.state.savedPet }
     savedPet[event.target.name] = event.target.value
     this.setState({ savedPet })
     console.log(savedPet)
@@ -89,67 +89,70 @@ export default class SearchBar extends Component {
 
 
   render() {
-   let mappedPets = this.state.searchresults.map((pet, i) => {
-     return (
-      <StyledUserContainer>
-      <div key={i}>
-      <div name='status' value={pet.status.$t}>
-      Status: {pet.status.$t}
-      </div>
-      <div name='age' value={pet.age.$t}>
-      Age: {pet.age.$t}
-      </div>
-      <div name='size' value={pet.size.$t}>
-      Size: {pet.size.$t}
-      </div>
-      <div name='name' value={pet.name.$t}>
-      Name: {pet.name.$t}
-      </div>
-      <div name='sex' value={pet.sex.$t}>
-      Sex: {pet.sex.$t}
-      </div>
-      <div name='description' value={pet.description.$t}>
-      Description: {pet.description.$t}
-      </div>
-      <div name='animal' value={pet.animal.$t}>
-      Animal: {pet.animal.$t}
-      </div>
-      <div name='contact' value={pet.contact.city.$t}>
-      City: {pet.contact.city.$t}
-      </div>
-      <div name='zipc' value={pet.contact.zip.$t}>
-      Zip Code: {pet.contact.zip.$t}
-      </div>
-      <div name='email' value={pet.contact.email.$t}>
-      Contact Email: {pet.contact.email.$t}
-      </div>
-      <button onClick={this.savePet}>Add Me To Your Favorites!</button>
-      </div>
-      </StyledUserContainer>
-     )
-   })
+    let mappedPets = this.state.searchresults.map((pet, i) => {
+      return (
+        <StyledUserContainer>
+          <div key={i}>
+          {pet.media.photos ? 
+            <img src={pet.media.photos.photo[1].$t}/> : 
+            <img src='www.placecage.com/'/> }
+            <div name='status' value={pet.status.$t}>
+              Status: {pet.status.$t}
+            </div>
+            <div name='age' value={pet.age.$t}>
+              Age: {pet.age.$t}
+            </div>
+            <div name='size' value={pet.size.$t}>
+              Size: {pet.size.$t}
+            </div>
+            <div name='name' value={pet.name.$t}>
+              Name: {pet.name.$t}
+            </div>
+            <div name='sex' value={pet.sex.$t}>
+              Sex: {pet.sex.$t}
+            </div>
+            <div name='description' value={pet.description.$t}>
+              Description: {pet.description.$t}
+            </div>
+            <div name='animal' value={pet.animal.$t}>
+              Animal: {pet.animal.$t}
+            </div>
+            <div name='contact' value={pet.contact.city.$t}>
+              City: {pet.contact.city.$t}
+            </div>
+            <div name='zipc' value={pet.contact.zip.$t}>
+              Zip Code: {pet.contact.zip.$t}
+            </div>
+            <div name='email' value={pet.contact.email.$t}>
+              Contact Email: {pet.contact.email.$t}
+            </div>
+            <button onClick={this.savePet}>Add Me To Your Favorites!</button>
+          </div>
+        </StyledUserContainer>
+      )
+    })
     return (
-    <div>
-      
-      <StyledSearch>
-       
-      <form onSubmit={this.onSubmit}> 
-      <input type='text' name='location' value={this.state.location} onChange={this.handleChange} placeholder='Zip Code'></input> 
-      <select onChange={this.handleChange} value={this.state.animal} name='animal'>
-        <option value="cat" onClick={this.handleChange} name="animal">Cats</option>
-        <option value="dog" onClick={this.handleChange} name="animal">Dogs</option>
-        <option value="rabbit" onClick={this.handleChange} name="animal">Rabbits</option>
-        <option value="bird" onClick={this.handleChange} name="animal">Birds</option>
-        <option value="horse" onClick={this.handleChange} name="animal">Horses</option>
-      </select>
-      <input type='submit' value='Search'/>
-      </form>
-      </StyledSearch>
-      <StyledMappedPets>
-     {mappedPets}
-      </StyledMappedPets>
+      <div>
+
+        <StyledSearch>
+
+          <form onSubmit={this.onSubmit}>
+            <input type='text' name='location' value={this.state.location} onChange={this.handleChange} placeholder='Zip Code'></input>
+            <select onChange={this.handleChange} value={this.state.animal} name='animal'>
+              <option value="cat" onClick={this.handleChange} name="animal">Cats</option>
+              <option value="dog" onClick={this.handleChange} name="animal">Dogs</option>
+              <option value="rabbit" onClick={this.handleChange} name="animal">Rabbits</option>
+              <option value="bird" onClick={this.handleChange} name="animal">Birds</option>
+              <option value="horse" onClick={this.handleChange} name="animal">Horses</option>
+            </select>
+            <input type='submit' value='Search' />
+          </form>
+        </StyledSearch>
+        <StyledMappedPets>
+          {mappedPets}
+        </StyledMappedPets>
       </div>
-    
+
     )
   }
 }
